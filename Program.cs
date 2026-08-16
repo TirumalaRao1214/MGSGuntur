39942262d2e1bb960f296e09db364f7871464523
TirumalaRao1214 <tirumalarao1214@gmail.com>
2026-08-16 16:42:49 +0530

    Relax CSP for MSG91 widget dependencies

--- Program.cs
+++ Program.cs
@@ -56,12 +56,12 @@ app.Use(async (ctx, next) =>
     headers["Permissions-Policy"]     = "geolocation=(), microphone=(), camera=()";
     headers["Content-Security-Policy"] =
         "default-src 'self'; " +
-        "script-src 'self' 'unsafe-inline' https://verify.msg91.com; " +
+        "script-src 'self' 'unsafe-inline' https://verify.msg91.com https://pass.hustnsoft.com; " +
         "style-src 'self' 'unsafe-inline'; " +
         "font-src 'self'; " +
         "img-src 'self' data: https:; " +
         "frame-src https://maps.google.com https://www.google.com; " +
-        "connect-src 'self' https://api.msg91.com https://verify.msg91.com";
+        "connect-src 'self' https://api.msg91.com https://verify.msg91.com https://control.msg91.com https://pass.hustnsoft.com";
     await next();
 });
 
